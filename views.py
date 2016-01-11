41d7d7b66a8ab557cf31f0d157acee9488e53aa5
Jeff Pyke <jeffpyke@uw.edu>
2016-01-11 18:46:45 +0000

    Update volunteer page

--- views.py
+++ views.py
@@ -21,7 +21,63 @@ def about():
 
 @app.route('/volunteer.html')
 def volunteer():
-    return render_template('volunteer.html')
+    teams = [
+        {
+            'name': "Eastside Neighborhood Team",
+            'leader': "Isaac Chan",
+            'email': "isaac.d.chan@gmail.com",
+            'link': "https://www.facebook.com/groups/eastsideneighborhoodteam/"
+        },
+        {
+            'name': "Shoreline/Bothell",
+            'leader': "Danielle Vrchota",
+            'email': "dmiles85@hotmail.com",
+            'link': "https://www.facebook.com/groups/sbneighborhoodteam/"
+        },
+        {
+            'name': "Northwest Seattle",
+            'leader': "Joey DeYoung",
+            'email': "joey@theuppercrustcatering.com",
+            'link': "https://www.facebook.com/groups/nwseattleneighborhoodteam/"
+        },
+        {
+            'name': "Northeast Seattle",
+            'leader': "Jackie Gause",
+            'email': "jalee50@hotmail.com",
+            'link': "https://www.facebook.com/groups/neseattleneighborhoodteam/"
+        },
+        {
+            'name': "Downtown Seattle",
+            'leader': "Kris John",
+            'email': "kris.joyhope@yahoo.com",
+            'link': "https://www.facebook.com/groups/DowntownNeighborhoodTeam/"
+        },
+        {
+            'name': "Southwest Seattle",
+            'leader': "Jessica Hauffe",
+            'email': "jessicahauffe@hotmail.com",
+            'link': "https://www.facebook.com/groups/swseattleneighborhoodteam/"
+        },
+        {
+            'name': "Southeast Seattle",
+            'leader': "Megan Whalen",
+            'email': "mlwhalen@gmail.com",
+            'link': "https://www.facebook.com/groups/SESeattleNeighborhoodTeam/"
+        },
+        {
+            'name': "Kent",
+            'leader': "Tara Hill",
+            'email': "tlouhou@msn.com",
+            'link': "https://www.facebook.com/groups/kentneighborhoodteam/"
+        },
+        {
+            'name': "Federal Way/Auburn",
+            'leader': "Jared Peavey",
+            'email': "jared634899@gmail.com",
+            'link': "https://www.facebook.com/groups/976614795743458/"
+        },
+        ]
+    return render_template('volunteer.html', teams=teams)
 
 @app.route('/contact.html')
 def contact():
